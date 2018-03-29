8dc5275a8acb5131247d564974718686edd7dc0c
Darren Matsumoto <bobafett@yahoo-inc.com>
2018-03-29 14:58:24 -0700

    fix: try this one

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -2,4 +2,8 @@ pkg_name=sd-cmd-validator
 pkg_origin=fenrirunbound
 pkg_version="0.1.0"
 pkg_scaffolding="core/scaffolding-node"
+scaffolding_pkg_manager=npm
 
+do_default_install() {
+  npm install -g screwdriver-command-validator
+}
